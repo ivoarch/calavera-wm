91fb1f22b04fac05958bd8e04c8de9a2646bd177
ivoarch <ivkuzev@gmail.com>
2013-03-01 11:30:59 +0100

    Added some useful information to color themes && Update white theme

--- themes/default.c
+++ themes/default.c
@@ -1,10 +1,15 @@
 /* Default theme */
 
-static const char normbordercolor[] = "#eee8d5";
-static const char normbgcolor[] = "#000000";
-static const char normfgcolor[] = "#eeeeee";
-static const char selbordercolor[] = "#383838";
-static const char selbgcolor[] = "#386596";
-static const char selfgcolor[] = "#eeeeee";
-static const char workspaces_bgcolor[] = "#202420";
-static const char workspaces_fgcolor[] = "#eeeeee";
+/* Borders */
+static const char normbordercolor[]    = "#eee8d5"; // border of inactiv window
+static const char selbordercolor[]     = "#383838"; // border of active window && workspaces area border
+
+/* Bar */
+static const char normbgcolor[]        = "#000000"; // color of bar
+static const char normfgcolor[]        = "#eeeeee"; // font color of bar
+
+/* Workspaces */
+static const char selbgcolor[]         = "#386596"; // background color of active workspace
+static const char selfgcolor[]         = "#eeeeee"; // font color of active workspace
+static const char workspaces_bgcolor[] = "#202420"; // color of workspace area
+static const char workspaces_fgcolor[] = "#eeeeee"; // font color of workspace area
--- themes/white.c
+++ themes/white.c
@@ -1,10 +1,15 @@
-/* White-theme */
+/* White theme */
 
-static const char normbordercolor[] = "#323232";
-static const char normbgcolor[] = "#d5d5d5";
-static const char normfgcolor[] = "#323232";
-static const char selbordercolor[] = "#E9E9E9";
-static const char selbgcolor[] = "#799A56";
-static const char selfgcolor[] = "#323232";
-static const char workspaces_bgcolor[] = "#969696";
-static const char workspaces_fgcolor[] = "#E9E9E9";
+/* Borders */
+static const char normbordercolor[]    = "#323232"; // border of inactiv window
+static const char selbordercolor[]     = "#E9E9E9"; // border of active window && workspaces area border
+
+/* Bar */
+static const char normbgcolor[]        = "#EDECEB"; // color of bar
+static const char normfgcolor[]        = "#323232"; // font color of bar
+
+/* Workspaces */
+static const char selbgcolor[]         = "#4E76A8"; // background color of active workspace
+static const char selfgcolor[]         = "#E9E9E9"; // font color of active workspace
+static const char workspaces_bgcolor[] = "#A9A5A2"; // color of workspace area
+static const char workspaces_fgcolor[] = "#E9E9E9"; // font color of workspace area
