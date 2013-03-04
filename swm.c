060095c5b93ee93904608e33d0f4f4add8b1df6e
ivoarch <ivkuzev@gmail.com>
2013-03-04 22:10:12 +0100

    Remove focus follow mouse

--- swm.c
+++ swm.c
@@ -919,7 +919,7 @@ void enternotify(XEvent *e) {
     Monitor *m;
     XCrossingEvent *ev = &e->xcrossing;
 
-    if((ev->mode != NotifyNormal || ev->detail == NotifyInferior) && ev->window != root)
+    //    if((ev->mode != NotifyNormal || ev->detail == NotifyInferior) && ev->window != root)
 	return;
     c = wintoclient(ev->window);
     m = c ? c->mon : wintomon(ev->window);
@@ -930,7 +930,6 @@ void enternotify(XEvent *e) {
     else if(!c || c == selmon->sel)
 	return;
     focus(c);
-    XRaiseWindow(display, m->sel->win);
 }
 
 void ewmh_init(void) {
