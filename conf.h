f0e11ef63aa782489849d83fe6ad9ae7b5941baa
ivoarch <ivkuzev@gmail.com>
2013-05-15 23:19:01 +0000

    default config fix

--- conf.h
+++ conf.h
@@ -2,7 +2,7 @@
 
 #define NORM_BORDERCOLOR     "black"       /* border of inactiv window */
 #define SEL_BORDERCOLOR      "SandyBrown"  /* border of active window */
-static const unsigned int padding  = 20;    /* gap at top of screen */
+static const unsigned int padding  = 0;    /* gap at top of screen */
 static const unsigned int snap     = 16;   /* snap pixel */
 static const unsigned int borderpx = 2;    /* border pixel of floating windows */
 static const Bool follow_mouse     = True; /* Focus the window with the mouse */
@@ -14,21 +14,21 @@ static const Bool waitkey          = True; /* Show the cursor when waiting for a
  */
 #define CURSOR_WAITKEY XC_icon
 
-#define N_WORKSPACES 4  /* Number of Workspaces */
+#define N_WORKSPACES 10  /* Number of Workspaces */
 
 /* key definitions */
 #define PREFIX_MODKEY ControlMask /* modifier prefix */
 #define PREFIX_KEYSYM XK_t  /* prefix key */
 
 #define WS_KEY(KEY,WS) \
-	{ None,                       KEY,      change_workspace,  {.ui = 1 << WS} }, \
+    { None,                       KEY,      change_workspace,  {.ui = 1 << WS} }, \
 	{ ShiftMask,                  KEY,      moveto_workspace,  {.ui = 1 << WS} },
 
 /* default commands */
-static const char *CMD_TERM[]  = { "urxvt", NULL };
+static const char *CMD_TERM[]    = { "urxvt", NULL };
 static const char *CMD_BROWSER[] = { "conkeror", NULL };
-static const char *CMD_EDITOR[] = { "emacsclient", "-n", "-c", "-a", "", NULL };
-static const char *CMD_LOCK[] = { "xlock", "-mode", "star", NULL };
+static const char *CMD_EDITOR[]  = { "emacsclient", "-n", "-c", "-a", "", NULL };
+static const char *CMD_LOCK[]    = { "xlock", "-mode", "star", NULL };
 
 /* KEY BINDINGS */
 static Key keys[] = {
@@ -55,6 +55,7 @@ static Key keys[] = {
     WS_KEY(                        XK_7,                      6) 
     WS_KEY(                        XK_8,                      7)
     WS_KEY(                        XK_9,                      8)
+    WS_KEY(                        XK_0,                      9)
 	{ ShiftMask,                  XK_r,      reload,         {0} },
     { ShiftMask,                  XK_q,      quit,           {0} },
 
@@ -62,9 +63,9 @@ static Key keys[] = {
 	{0, XF86XK_AudioLowerVolume,
         spawn, {.v = (const char*[]){"amixer", "-q", "-c", "0", "set", "Master", "5-", "unmute", NULL}}},
     {0, XF86XK_AudioRaiseVolume,
-	    spawn, {.v = (const char*[]){"amixer", "-q", "-c", "0", "set", "Master", "5+", "unmute", NULL}}},
+        spawn, {.v = (const char*[]){"amixer", "-q", "-c", "0", "set", "Master", "5+", "unmute", NULL}}},
     {0, XF86XK_AudioMute,
-	    spawn, {.v = (const char*[]){"amixer", "-q", "-c", "0", "set", "Master", "toggle", NULL}}},
+        spawn, {.v = (const char*[]){"amixer", "-q", "-c", "0", "set", "Master", "toggle", NULL}}},
 };
 
 /* BUTTONS*/
@@ -72,5 +73,5 @@ static Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkClientWin,         ControlMask,    Button1,        movemouse,      {0} },
 	{ ClkClientWin,         ControlMask,    Button3,        resizemouse,    {0} },
-        { ClkClientWin,         ControlMask,    Button2,        killfocused,    {0} },
+    { ClkClientWin,         ControlMask,    Button2,        killfocused,    {0} },
 };
